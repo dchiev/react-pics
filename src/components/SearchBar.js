3617555396356f00e240fa01826a8ff040816b94
Dan Chiev <danchiev2@Gmail.com>
2021-07-18 12:17:36 -0400

    added state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,18 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
-  onInputClick() {
-    console.log("input was clicked ");
-  }
+  state = { term: "" };
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={(e) => console.log(e.target.value)} />
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
